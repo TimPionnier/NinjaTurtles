48e14ccf5f5d188ce64bb3d0a648bc2af1fba3cc
Theo Lartinez <theomartinez2@gmail.com>
2020-01-06 16:58:47 +0100

    Création méthode tour + classe Deck

---
+++ src/com/company/Deck.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+
+    private ArrayList<Character> pioche = new ArrayList<>();
+    private ArrayList<Character> murs = new ArrayList<>();
+    private ArrayList<Character> defausse = new ArrayList<>();
+    private ArrayList<Character> main = new ArrayList<>();
+
+    public Deck() {
+        //Remplissage de la pioche
+        for (int i=0;i<18;i++){
+            this.pioche.add('B'); //Carte Bleue
+        }
+        for (int i=0;i<8;i++){
+            this.pioche.add('J'); //Carte Jaune
+            this.pioche.add('V'); //Carte Violette
+        }
+        for (int i=0;i<3;i++){
+            this.pioche.add('L'); //Carte Laser
+            this.murs.add('P');   //Mur de pierre
+        }
+        for (int i=0;i<2;i++){
+            this.murs.add('G');   //Mur de glace
+        }
+
+        //Mélange des cartes
+        this.pioche = shuffle(this.pioche);
+
+        //Distribution main
+        for (int i=0;i<5;i++){
+            this.main.add(this.pioche.get(0));
+            this.pioche.remove(0);
+        }
+    }
+
+    public static ArrayList<Character> shuffle (ArrayList<Character> liste){
+        Random rand = new Random();
+        for (int i=0;i<liste.size();i++){
+            int randomIndex = rand.nextInt(liste.size());
+            Character temp = liste.get(randomIndex);
+            liste.set(randomIndex,liste.get(i));
+            liste.set(i,temp);
+        }
+        return liste;
+    }
+}
