3eb513828beafb346869038a244771b825c2569c
Theo Lartinez <theomartinez2@gmail.com>
2020-01-22 12:13:09 +0100

    Plateau.java clean

--- src/com/company/Plateau.java
+++ src/com/company/Plateau.java
@@ -1,33 +1,27 @@
 package com.company;
 
-import org.newdawn.slick.Game;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Plateau {
-    private final int NB_CASES = 64;
     private static ArrayList<Case> cases;
 
-    public Plateau() throws SlickException {
+    public Plateau() {
 
-        this.cases = new ArrayList<>();
+        cases = new ArrayList<>();
         int k = 0;
         for (int i = 0; i <= 7; i++) {
             for (int j = 0; j <= 7; j++) {
-                this.cases.add(k, new Case(i, j, ' '));
+                cases.add(k, new Case(i, j, ' '));
                 k++;
             }
         }
 
     }
 
+    //Retourne la case en fonction de sa position sur le plateau
     public static Case getCase(int x, int y) {
         for (Case current : cases) {
-            if (current.getPosition(0) == x && current.getPosition(1) == y){
+            if (current.getPosition(0) == x && current.getPosition(1) == y) {
                 return current;
             }
         }
@@ -42,28 +36,28 @@ public class Plateau {
         switch (nbrJoueur) {
             case 2:
                 //Affichage du joyau
-                getCase(7,3).setEtat('?');
+                getCase(7, 3).setEtat('?');
 
                 //Affichage des caisses en bois
                 for (int i = 0; i < 8; i++) {
-                    getCase(i,7).setEtat('C');
+                    getCase(i, 7).setEtat('C');
                 }
                 break;
             case 3:
                 //Affichage du joyau
-                getCase(7,0).setEtat('?');
-                getCase(7,3).setEtat('?');
-                getCase(7,6).setEtat('?');
+                getCase(7, 0).setEtat('?');
+                getCase(7, 3).setEtat('?');
+                getCase(7, 6).setEtat('?');
 
                 //Affichage des caisses en bois
                 for (int i = 0; i < 8; i++) {
-                    getCase(i,7).setEtat('C');
+                    getCase(i, 7).setEtat('C');
                 }
                 break;
             case 4:
                 //Affichage du joyau
-                getCase(7,1).setEtat('?');
-                getCase(7,6).setEtat('?');
+                getCase(7, 1).setEtat('?');
+                getCase(7, 6).setEtat('?');
 
         }
 
